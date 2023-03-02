e67eb0465f5a69456c6e3d9f2d08fde66f9d4a62
José Manuel Vega Gradit <67061927+Josemvg@users.noreply.github.com>
2023-03-02 21:44:28 +0100

    Added model comparison table

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,22 @@
+from collections import defaultdict
 from sklearn.decomposition import PCA
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+from sklearn.metrics import (
+    mean_squared_error, 
+    r2_score, mean_absolute_error, 
+    silhouette_score, 
+    classification_report, 
+    RocCurveDisplay,
+    f1_score,
+    brier_score_loss,
+    log_loss,
+    precision_score,
+    recall_score,
+    accuracy_score
+)
 
 def fit_pca(df, n_components=2, preprocessor=None):
     """
@@ -132,3 +146,115 @@ def plot_loadings(loadings, components):
             plt.xticks(rotation=90)
     plt.show()
     return
+
+def get_classification_metrics(models: dict, X_test, y_test, metrics, X_train=None, y_train=None, X_test_pca = None, X_train_pca = None):
+    """
+    Function that returns a metric table for a list of Classification models. Including:
+    - Brier Loss
+    - Log Loss
+    - Precision
+    - Recall
+    - F1-Score
+    - Accuracy
+
+    Parameters
+    ----------
+    models : dict
+        Dictionary with the model name and the model
+    X_test : array-like
+        Test data
+    y_test : array-like, 1d
+        Test labels
+    metrics: list of str
+        List of metrics to calculate. Possible values are 'brier_loss', 'log_loss', 'precision', 'recall', 'f1_score', 'accuracy'
+    X_train : array-like, optional
+        Train data
+    y_train : array-like, 1d, optional
+        Train labels
+    
+    Returns
+    -------
+    metric_table : pandas DataFrame
+        DataFrame with the metrics for each model
+    """
+
+    # Create a dictionary with the metrics
+    scores = defaultdict(list)
+    # Initialize the flags as false:
+    train_flag = False
+    pca_flag = False
+    # Iterate over the models
+    for name, model in models.items():
+        if 'pca' in name.lower():
+            pca_flag = True
+            y_pred_pca = model.predict(X_test_pca)
+            # Calculate the metrics for the train data
+            if X_train is not None and y_train is not None:
+                train_flag = True
+                y_pred_train_pca = model.predict(X_train_pca)
+        else:
+            pca_flag = False
+            y_pred = model.predict(X_test)
+            # Calculate the metrics for the train data
+            if X_train is not None and y_train is not None:
+                train_flag = True
+                y_pred_train = model.predict(X_train)
+        scores["Classifier"].append(name)
+        # Iterate over the metrics
+        for metric_name in metrics:
+            if pca_flag:
+                # Calculate the metrics for the test data
+                score = get_score(metric_name, y_test, y_pred_pca)
+                # Append the score to the scores dictionary
+                scores[f"{metric_name.capitalize().replace('_', ' ')}"].append(score)
+                if train_flag:
+                    # Calculate the metrics for the train data
+                    score_train = get_score(metric_name, y_train, y_pred_train_pca)
+                    # Append the score to the scores dictionary
+                    scores[f"{metric_name.capitalize().replace('_', ' ')} (Train)"].append(score_train)
+            else:    
+                # Calculate the metrics for the test data
+                score = get_score(metric_name, y_test, y_pred)
+                # Append the score to the scores dictionary
+                scores[f"{metric_name.capitalize().replace('_', ' ')}"].append(score)
+                if train_flag:
+                    # Calculate the metrics for the train data
+                    score_train = get_score(metric_name, y_train, y_pred_train)
+                    # Append the score to the scores dictionary
+                    scores[f"{metric_name.capitalize().replace('_', ' ')} (Train)"].append(score_train)
+
+    # Create a DataFrame with the metrics
+    metric_table = pd.DataFrame(scores).set_index("Classifier")
+    # Round the values of the DataFrame
+    metric_table.round(decimals=3)
+
+    return metric_table
+
+def get_score(metric_name, y_true, y_pred):
+    """
+    A nested function to calculate different scores based on the metric name
+
+    Parameters
+    ----------
+    metric_name : str
+        The name of the metric to calculate
+    y_true : array-like, 1d
+        The true labels
+    y_pred : array-like, 1d
+        The predicted labels
+
+    Returns
+    -------
+    score : float
+        The score for the given metric
+    """
+    metric_funcs = {
+        'brier_loss':brier_score_loss, 
+        'precision':precision_score, 
+        'recall':recall_score, 
+        'f1_score':f1_score, 
+        'accuracy':accuracy_score
+    }
+    metric_func = metric_funcs.get(metric_name, precision_score)
+    score = metric_func(y_true, y_pred)
+    return score
