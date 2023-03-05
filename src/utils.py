bd12582447ee0a0973afffcd83848926c03c544a
José Manuel Vega Gradit <67061927+Josemvg@users.noreply.github.com>
2023-03-05 13:05:52 +0100

    Added calib curves and predicted proba comparison

--- src/utils.py
+++ src/utils.py
@@ -18,6 +18,7 @@ from sklearn.metrics import (
     accuracy_score
 )
 
+
 def fit_pca(df, n_components=2, preprocessor=None):
     """
     Fits a PCA model to the dataframe
@@ -42,6 +43,7 @@ def fit_pca(df, n_components=2, preprocessor=None):
     pca = PCA(n_components=n_components)
     df_pca = pca.fit_transform(df)
     return pca, df_pca
+
 
 def get_explained_variance(pca, plot=True):
     """
@@ -79,6 +81,7 @@ def get_explained_variance(pca, plot=True):
 
     return exp_variance
 
+
 def get_n_components(exp_variance, threshold):
     """
     Get the number of components that explain a given threshold of the variance
@@ -98,6 +101,7 @@ def get_n_components(exp_variance, threshold):
     n_components = exp_variance[exp_variance['cum_Exp_variance'] >= threshold].index[0]
     n_components = int(n_components.replace('PC', ''))
     return n_components
+
 
 def get_loadings(pca, colNames):
     """
@@ -121,6 +125,7 @@ def get_loadings(pca, colNames):
         columns = ['PC' + str(n_pca + 1) for n_pca in range(pca.n_components)], 
         index = colNames)
     return loadings
+
 
 def plot_loadings(loadings, components):
     """
@@ -146,6 +151,7 @@ def plot_loadings(loadings, components):
             plt.xticks(rotation=90)
     plt.show()
     return
+
 
 def get_classification_metrics(models: dict, X_test, y_test, metrics, X_train=None, y_train=None, X_test_pca = None, X_train_pca = None):
     """
@@ -230,6 +236,7 @@ def get_classification_metrics(models: dict, X_test, y_test, metrics, X_train=No
 
     return metric_table
 
+
 def get_score(metric_name, y_true, y_pred):
     """
     A nested function to calculate different scores based on the metric name
@@ -258,3 +265,131 @@ def get_score(metric_name, y_true, y_pred):
     metric_func = metric_funcs.get(metric_name, precision_score)
     score = metric_func(y_true, y_pred)
     return score
+
+
+def plot_calibration_curves_comparison(models: dict, X_test, y_test):
+    """
+    Plots the calibration curve and histograms for multiple models using Plotly.
+    The different models are plotted in different colors and can be turned on and off by clicking on the legend.
+
+    Parameters
+    ----------
+    models : dict
+        A dictionary of the models to plot. Each key should be a string with the name of the model,
+        and each value should be an estimator with a `predict_proba` method that takes `X_test` as input.
+    X_test : array-like
+        Test data
+    y_test : array-like, 1d
+        Test labels
+
+    Returns
+    -------
+    None
+    """
+    # Define the colors for the plots
+    colors = px.colors.qualitative.Dark2
+
+    # Create a subplot for the calibration curve
+    fig = make_subplots(rows=1, cols=1)
+    fig.update_layout(title_text="Calibration Curves for the models")
+    # Make sure the x and y axes are the same scale
+    fig.update_xaxes(range=[0, 1])
+    fig.update_yaxes(range=[0, 1])
+
+    # Plot the calibration curves for the models
+    for i, (name, model) in enumerate(models.items()):
+        # Get the predicted probabilities for the test data
+        y_prob = model.predict_proba(X_test)[:, 1]
+        # Get the true and predicted probabilities for the calibration curve
+        true_prob, pred_prob = calibration_curve(y_test, y_prob, n_bins=10)
+        # Plot the calibration curve
+        trace = go.Scatter(x=pred_prob, y=true_prob, mode='lines+markers', name=name, line=dict(color=colors[i]))
+        fig.add_trace(trace, row=1, col=1)
+
+    # Add a diagonal line going through y=x
+    diag_trace = go.Scatter(x=[0, 1], y=[0, 1], mode='lines', name='Ideal', line=dict(color='black', dash='dash'))
+    fig.add_trace(diag_trace, row=1, col=1)
+
+    # Add the x and y axis labels
+    fig.update_xaxes(title_text='Predicted probability', row=1, col=1)
+    fig.update_yaxes(title_text='True probability', row=1, col=1)
+    # Add the legend and set the figure size
+    fig.update_layout(showlegend=True, legend_title_text='Model', width=800, height=650)
+    fig.show()
+    return
+
+
+def plot_predicted_probabilities_comparison(models, X_train, y_train, X_test, y_test):
+    """
+    Plots the predicted probabilities for each model in the dictionary. 
+    Each model will have two plots, one for the training set and one for the test set.
+    On each plot, the predicted probabilities are plotted for the positive and the negative class
+    (positive in orange, negative in blue)
+
+    Parameters
+    ----------
+    models : dict
+        Dictionary of models to plot. The key is the name of the model and the value is the model object
+    X_train : array-like
+        Training data
+    y_train : array-like, 1d
+        Training labels
+    X_test : array-like
+        Test data
+    y_test : array-like, 1d
+        Test labels
+    """
+    # Create the figure and axes
+    fig, axs = plt.subplots(len(models), 2, figsize=(10, len(models)*4.75))
+
+    # Loop over each model and create the plots
+    for i, (name, model) in enumerate(models.items()):
+        # Obtain the predicted probabilities for the training set
+        y_train_pred = model.predict_proba(X_train)[:, 1]
+        neg_train_idx = np.where(y_train == 0)[0]
+        pos_train_idx = np.where(y_train == 1)[0]
+        # Convert the predicted probabilities into a histogram for the negative and positive class
+        neg_train_counts, _ = np.histogram(y_train_pred[neg_train_idx], bins=np.linspace(0, 1, 11))
+        pos_train_counts, _ = np.histogram(y_train_pred[pos_train_idx], bins=np.linspace(0, 1, 11))
+        # Calculate the percentage of the positive and negative class in each bin
+        neg_train_perc = neg_train_counts / len(neg_train_idx) * 100
+        pos_train_perc = pos_train_counts / len(pos_train_idx) * 100
+
+        # Plot the training set histogram for each class
+        axs[i, 0].plot(np.linspace(0, 1, 10), neg_train_perc, '-o', label='Negative')
+        axs[i, 0].plot(np.linspace(0, 1, 10), pos_train_perc, '-o', label='Positive')
+        # Set the labels and title
+        axs[i, 0].set_xlabel('Predicted probability')
+        axs[i, 0].set_ylabel('Percentage')
+        axs[i, 0].set_ylim([0, 100])
+        axs[i, 0].set_title(f'{name} (train set)')
+        # Add the legend
+        axs[i, 0].legend()
+
+        # Obtain the predicted probabilities for the test set
+        y_test_pred = model.predict_proba(X_test)[:, 1]
+        neg_test_idx = np.where(y_test == 0)[0]
+        pos_test_idx = np.where(y_test == 1)[0]
+        # Convert the predicted probabilities into a histogram for the negative and positive class
+        neg_test_counts, _ = np.histogram(y_test_pred[neg_test_idx], bins=np.linspace(0, 1, 11))
+        pos_test_counts, _ = np.histogram(y_test_pred[pos_test_idx], bins=np.linspace(0, 1, 11))
+        # Calculate the percentage of the positive and negative class in each bin
+        neg_test_perc = (neg_test_counts / len(neg_test_idx)) * 100
+        pos_test_perc = (pos_test_counts / len(pos_test_idx)) * 100
+
+        # Plot the training set histogram for each class
+        axs[i, 1].plot(np.linspace(0, 1, 10), neg_test_perc, '-o', label='Negative')
+        axs[i, 1].plot(np.linspace(0, 1, 10), pos_test_perc, '-o', label='Positive')
+        # Set the labels and title
+        axs[i, 1].set_xlabel('Predicted probability')
+        axs[i, 1].set_ylabel('Percentage')
+        axs[i, 1].set_ylim([0, 100])
+        axs[i, 1].set_title(f'{name} (test set)')
+        # Add the legend
+        axs[i, 1].legend()
+
+    # Set the spacing between the plots
+    plt.tight_layout()
+    # Show the plot
+    plt.show()
+    return
